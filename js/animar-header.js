2f7d75afcfc91b77044c83a114cde255100406bc
joaoteixeira_ <jp2887860@gmail.com>
2025-03-16 02:32:16 -0300

    Initial commit

---
+++ js/animar-header.js
@@ -0,0 +1,8 @@
+const menuItems = document.querySelectorAll("#sobre, #projetos, #contato");
+
+menuItems.forEach(item => {
+    item.addEventListener("click", () => {
+        menuItems.forEach(i => i.classList.remove("active"));
+        item.classList.add("active");
+    });
+});
