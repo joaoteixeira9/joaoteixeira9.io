b808516190becfec515832e9fead1d640ee86350
joaoteixeira_ <jp2887860@gmail.com>
2025-03-20 23:19:22 -0300

    Responsividade seções "Sobre" e "Projetos"

--- js/app.js
+++ js/app.js
@@ -7,3 +7,49 @@ menuItems.forEach(item => {
         item.classList.add("active");
     });
 });
+
+function Responsividade() {
+    const sobre = document.getElementById("sobre");
+    const div = document.getElementById("1div");
+    const cards = document.querySelectorAll(".card-sobre");
+    const cardp = document.querySelectorAll(".card-projeto");
+    const itemp = document.querySelectorAll(".item-projeto");
+
+    if (window.innerWidth <= 768) {
+        sobre.classList.add('.');
+        sobre.classList.remove('d-flex', 'justify-content-around', 'mb-5');
+        div.classList.add('d-inline', 'w-100')
+        div.classList.remove('d-flex','justify-content-around', 'w-75');
+        cards.forEach(card => {
+            card.classList.add('align-items-center', 'text-center', 'mb-5');
+            card.classList.remove('w-25', 'border-end');
+        });
+        cardp.forEach(card => {
+            card.classList.add('flex-column', 'align-items-center');
+            card.classList.remove('justify-content-around')
+        })
+        itemp.forEach(item => {
+            item.classList.add('mb-5')
+        })
+    } else {
+        sobre.classList.add('d-flex', 'justify-content-around', 'mb-5');
+        sobre.classList.remove('sobre');
+        div.classList.remove('d-inline', 'w-100');
+        div.classList.add('d-flex','justify-content-around', 'w-75');
+        cards.forEach(card => {
+            card.classList.remove('align-items-center', 'text-center', 'mb-5');
+            card.classList.add('w-25');
+        });
+        cardp.forEach(card => {
+            card.classList.remove('flex-column', 'align-items-center');
+            card.classList.add('justify-content-around')
+        })
+        itemp.forEach(item => {
+            item.classList.remove('mb-5')
+        })
+    }
+}
+
+window.addEventListener('resize', Responsividade);
+
+Responsividade();
